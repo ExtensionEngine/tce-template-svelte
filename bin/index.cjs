982e3af63519886b961ad1ca6051b793d4c4b849
Marin Skorsur <m.skorsur@gmail.com>
2022-08-12 12:01:31 +0200

    Prettify output

--- bin/index.cjs
+++ bin/index.cjs
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+const chalk = require('chalk');
 const { execFileSync } = require('child_process');
 const shell = require('shelljs');
 
@@ -6,8 +7,16 @@ const SUCCESS_CODE = 0;
 const ERROR_CODE = 1;
 
 function exitOnError(errorMessage) {
-  shell.echo(errorMessage);
+  shell.echo(prettifyErrorMessage(errorMessage));
   shell.exit(ERROR_CODE);
+}
+
+function prettifyStepTitle(title) {
+  return chalk.bgGreen.bold(title);
+}
+
+function prettifyErrorMessage(errorMessage) {
+  return chalk.bgRed.bold(errorMessage);
 }
 
 if (!shell.which('git')) {
@@ -22,17 +31,17 @@ if (!shell.which('node') || !shell.which('npm')) {
   exitOnError('Sorry, this script requires node & npm');
 }
 
-shell.echo('Cloning respository');
+shell.echo(prettifyStepTitle('1. Cloning respository'));
 if (shell.exec('degit https://github.com/ExtensionEngine/tailor-content-element').code !== SUCCESS_CODE) {
   exitOnError('Cloning respository via degit failed');
 }
 
-shell.echo('Installing dependencies');
+shell.echo(prettifyStepTitle('2. Installing dependencies'));
 if (shell.exec('npm install').code !== SUCCESS_CODE) {
   exitOnError('Installing dependencies via npm failed');
 }
 
-shell.echo('Setting up project');
+shell.echo(prettifyStepTitle('3. Setting up project'));
 try {
   execFileSync('npm', ['run', 'setup'], { stdio: 'inherit' });
 } catch {
