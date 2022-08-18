aa84e84c80b5592f94af6d97c641ac9a76c8bbac
Marin Skorsur <m.skorsur@gmail.com>
2022-08-18 10:43:30 +0200

    Extract functions

--- bin/index.cjs
+++ bin/index.cjs
@@ -24,6 +24,39 @@ function prettifyErrorMessage(errorMessage) {
   return chalk.bgRed.bold(errorMessage);
 }
 
+async function cloneRepository() {
+  shell.echo(prettifyStepTitle('1/3 Cloning respository'));
+  const name = await prompt('Enter the name of the project to create: ');
+
+  const cloneCommand = shell.exec(`git clone --depth 1 https://github.com/ExtensionEngine/tce-template-svelte ${name}`);
+  shell.cd(`./${name}`);
+  shell.exec('rm -rf .git');
+
+  const pkgJson = await PackageJson.load('./');
+  pkgJson.update({ name });
+  await pkgJson.save();
+
+  if (cloneCommand.code !== SUCCESS_CODE) {
+    exitOnError('Cloning respository failed');
+  }
+}
+
+async function installDependencies() {
+  shell.echo(prettifyStepTitle('2/3 Installing dependencies'));
+  if (shell.exec('npm install').code !== SUCCESS_CODE) {
+    exitOnError('Installing dependencies via npm failed');
+  }
+}
+
+async function runSetup() {
+  shell.echo(prettifyStepTitle('3/3 Setting up project'));
+  try {
+    execFileSync('npm', ['run', 'setup'], { stdio: 'inherit' });
+  } catch {
+    exitOnError('Project setup failed');
+  }
+}
+
 async function executeScript() {
   if (!shell.which('git')) {
     exitOnError('Sorry, this script requires git');
@@ -33,30 +66,9 @@ async function executeScript() {
     exitOnError('Sorry, this script requires node & npm');
   }
 
-  shell.echo(prettifyStepTitle('1/3 Cloning respository'));
-  const name = await prompt('Enter the name of the project to create: ');
-  const cloneCommand = shell.exec(`git clone --depth 1 https://github.com/ExtensionEngine/tce-template-svelte ${name}`);
-  shell.cd(`./${name}`);
-  shell.exec('rm -rf .git');
-
-  const pkgJson = await PackageJson.load('./');
-  pkgJson.update({ name });
-  await pkgJson.save();
-  if (cloneCommand.code !== SUCCESS_CODE) {
-    exitOnError('Cloning respository failed');
-  }
-
-  shell.echo(prettifyStepTitle('2/3 Installing dependencies'));
-  if (shell.exec('npm install').code !== SUCCESS_CODE) {
-    exitOnError('Installing dependencies via npm failed');
-  }
-
-  shell.echo(prettifyStepTitle('3/3 Setting up project'));
-  try {
-    execFileSync('npm', ['run', 'setup'], { stdio: 'inherit' });
-  } catch {
-    exitOnError('Project setup failed');
-  }
+  await cloneRepository();
+  await installDependencies();
+  await runSetup();
 
   shell.exit(SUCCESS_CODE);
 }
